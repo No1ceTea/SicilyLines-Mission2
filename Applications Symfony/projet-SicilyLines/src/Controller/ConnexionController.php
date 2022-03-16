fafd33d65b93c57559a65828407b6b0f0f0c3121
Noam-Oak <ouakninenoam.pro@gmail.com>
2022-03-16 10:18:47 +0100

    Update : Dossier Symfony
    Contrôleurs : ✓ (Accueil | Connexion | Inscription | Contact )
    Base de données : ✓

---
+++ Applications Symfony/projet-SicilyLines/src/Controller/ConnexionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class ConnexionController extends AbstractController
+{
+    public function index(): Response
+    {
+        return $this->render('connexion/index.html.twig', [
+            'controller_name' => 'ConnexionController',
+        ]);
+    }
+}
